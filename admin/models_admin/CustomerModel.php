3bb71d5a47d7c560c4a2c2c510669a96bbd14109
Nam-th <th.nam1112@gmail.com>
2025-04-11 16:18:10 +0700

    feat: crud products

--- admin/models_admin/CustomerModel.php
+++ admin/models_admin/CustomerModel.php
@@ -1,28 +1,33 @@
 <?php
-    class CustomerModel {
+require_once "../config/Database.php";
 
+    class CustomerModel {
+        private $db;
+        public function __construct() {
+            $this->db = new Database();
+        }
         public function select_users() {
             $sql = "SELECT username, full_name, email, phone FROM users";
 
-            return pdo_query($sql);
+            return $this->db->query($sql);
         }
 
         public function select_all_users() {
             $sql = "SELECT * FROM users ORDER BY user_id DESC";
 
-            return pdo_query($sql);
+            return $this->db->query($sql);
         }
 
         public function user_insert($username, $password, $full_name, $image, $email, $phone, $address, $role) {
             $sql = "INSERT INTO users(username, password, full_name, image, email, phone, address, role) VALUES(?,?,?,?,?,?,?,?)";
 
-            pdo_execute($sql, $username, $password, $full_name, $image, $email, $phone, $address, $role);
+            $this->db->execute($sql, $username, $password, $full_name, $image, $email, $phone, $address, $role);
         }
 
         public function get_user_admin($username) {
             $sql = "SELECT * FROM users WHERE username = ? AND role = 1";
 
-            return pdo_query($sql, $username);
+            return $this->db->query($sql, $username);
         }
 
     }
